9555d9490c3c9e0891514f30743af61319ddb736
WildElaeagnus <54314123+akorzunin@users.noreply.github.com>
2022-03-11 15:35:54 +0300

    add utils

---
+++ utils/alg_utils.py
@@ -0,0 +1,18 @@
+import logging
+def calculate_profit(trade_data):
+    for i, row in trade_data.iterrows():
+    # try:
+        if row['type'] == 'sell':
+            try:
+                trade_data.loc[i, 'profit_abs'] = float(row['USD']) - float(trade_data['USD'].loc[i-2])
+                # (curr_buy_pr - prev_buy_pr)*100/prev_buy_pr
+                trade_data.loc[i, 'profit_rel'] = round((float(row['USD']) - float(trade_data['USD'].loc[i-2])) *100/float(trade_data['USD'].loc[i-2]),2)
+            except KeyError: logging.debug(f'[CALC PROFIT] cannot find index: {i}')
+        if row['type'] == 'buy':
+            try:
+                trade_data.loc[i, 'profit_abs'] = float(row['RVN']) - float(trade_data['RVN'].loc[i-2])
+                # (curr_buy_pr - prev_buy_pr)*100/prev_buy_pr
+                trade_data.loc[i, 'profit_rel'] = round((float(row['RVN']) - float(trade_data['RVN'].loc[i-2]))*100/float(trade_data['RVN'].loc[i-2]),2)
+            except KeyError: logging.debug(f'[CALC PROFIT]cannot find index: {i}')
+    
+    return trade_data
